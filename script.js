e710fd906cd065226c8588b853e78b15dc152d3d
MrP4l <101936971+MrP4l@users.noreply.github.com>
2022-12-16 18:07:00 +0100

    Little changes

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+let addBook = document.getElementById("addInput");
+let title = document.getElementById("bookTitleInput");
+let author = document.getElementById("authorInput");
+let pages = document.getElementById("pagesInput");
+let read = document.getElementById("readInput");
 let myLibrary = [];
 
-function Book() {
-    // the constructor...
-  }
-  
-  function addBookToLibrary() {
-    // do stuff here
-  }
+
+
+addBook.addEventListener("click", preventSubmitting);
+function preventSubmitting(event) {
+  event.preventDefault();
+}
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read
+}
+
+addBook.addEventListener("click", () => {
+  /*myLibrary.push(title.value, author.value, pages.value, read.value)
+  console.log(myLibrary)*/
+
+});
+
+function addBookToLibrary() {
+}
 
 
 
